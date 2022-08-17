a1a697eaa87af447602d38531d3ab3e92572a841
maimium <mamopoyoo@yahoo.co.jp>
2022-08-17 14:48:44 +0900

    商品出品機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true
-  validates :password,                            format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }#, message: "は半角英数混合で入力してください"}
-  validates :family_name,        presence: true,  format: { with:/\A[ぁ-んァ-ン一-龥々ー]+\z/ }#, message: "は全角ひらがな/カタカナまたは漢字で入力してください"}
-  validates :personal_name,      presence: true,  format: { with:/\A[ぁ-んァ-ン一-龥々ー]+\z/ }#, message: "は全角ひらがな/カタカナまたは漢字で入力してください"}
-  validates :family_name_kana,   presence: true,  format: { with:/\A[ァ-ヶー－]+\z/ }#, message: "は全角カタカナで入力してください"}
-  validates :personal_name_kana, presence: true,  format: { with:/\A[ァ-ヶー－]+\z/ }#, message: "は全角カタカナで入力してください"}
+  validates :nickname, presence: true
+  validates :password,                            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :family_name,        presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :personal_name,      presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :family_name_kana,   presence: true,  format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :personal_name_kana, presence: true,  format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,           presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-      
+
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password { '1a'+Faker::Internet.password(min_length: 6) }
+    password { '1a' / Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     family_name { Gimei.last.kanji }
     family_name_kana { Gimei.last.katakana }
