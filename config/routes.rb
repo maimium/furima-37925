444c726cce9f8d584da66b6238254622a8629274
maimium <mamopoyoo@yahoo.co.jp>
2022-08-21 15:10:25 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
 
 end
