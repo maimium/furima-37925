10411f233c8bc42e76e0f59b58747baac5307127
maimium <mamopoyoo@yahoo.co.jp>
2022-08-23 17:34:07 +0900

    商品情報編集機能(修正済)
    コントローラーに複数回出てくる記述をbefore_actionで呼び出すようにし、見やすいコードへ修正しました。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_index, except: [:index, :show]
+  before_action :product_page, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -20,18 +21,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     unless @product.user == current_user
       redirect_to root_path
     end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -43,6 +41,10 @@ class ProductsController < ApplicationController
 
   def move_to_index
     redirect_to index unless user_signed_in?
+  end
+
+  def product_page
+    @product = Product.find(params[:id])
   end
 
   def product_params
